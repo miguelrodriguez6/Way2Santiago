bd0ce5516de39b11ed90e76664d23546bad0099a
miguelrodriguez6 <rodriguez.miguel4300@gmail.com>
2024-12-15 23:56:46 +0100

    feat: add jwt token functionality to already existing endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\StageCommentsController;
 use App\Http\Controllers\Api\StageController;
 use App\Http\Controllers\Api\StageParticipantsController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +20,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth')->get('/users', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::prefix('users')->group(function() {
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'users'
+
+], function ($router) {
     Route::get('/', [UserController::class, 'index']); // GET /api/users
     Route::post('/', [UserController::class, 'store']); // POST /api/users
     Route::get('{id}', [UserController::class, 'show']); // GET /api/users/{id}
@@ -31,7 +37,12 @@ Route::prefix('users')->group(function() {
     Route::delete('{id}', [UserController::class, 'destroy']); // DELETE /api/users/{id}
 });
 
-Route::prefix('locations')->group(function() {
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'locations'
+
+], function ($router) {
     Route::get('/', [LocationController::class, 'index']); // GET /api/locations
     Route::post('/', [LocationController::class, 'store']); // POST /api/locations
     Route::get('{id}', [LocationController::class, 'show']); // GET /api/locations/{id}
@@ -39,7 +50,12 @@ Route::prefix('locations')->group(function() {
     Route::delete('{id}', [LocationController::class, 'destroy']); // DELETE /api/locations/{id}
 });
 
-Route::prefix('stages')->group(function() {
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'stages'
+
+], function ($router) {
     Route::get('/', [StageController::class, 'index']); // GET /api/stages
     Route::post('/', [StageController::class, 'store']); // POST /api/stages
     Route::get('{id}', [StageController::class, 'show']); // GET /api/stages/{id}
@@ -47,7 +63,12 @@ Route::prefix('stages')->group(function() {
     Route::delete('{id}', [StageController::class, 'destroy']); // DELETE /api/stages/{id}
 });
 
-Route::prefix('stage-comments')->group(function() {
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'stage-comments'
+
+], function ($router) {
     Route::get('/', [StageCommentsController::class, 'index']); // GET /api/stage-comments
     Route::post('/', [StageCommentsController::class, 'store']); // POST /api/stage-comments
     Route::get('{id}', [StageCommentsController::class, 'show']); // GET /api/stage-comments/{id}
@@ -55,11 +76,29 @@ Route::prefix('stage-comments')->group(function() {
     Route::delete('{id}', [StageCommentsController::class, 'destroy']); // DELETE /api/stage-comments/{id}
 });
 
-Route::prefix('participants')->group(function() {
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'participants'
+
+], function ($router) {
     Route::get('/', [StageParticipantsController::class, 'index']); // GET /api/participants
     Route::post('/', [StageParticipantsController::class, 'store']); // POST /api/participants
     Route::get('{id}', [StageParticipantsController::class, 'show']); // GET /api/participants/{id}
     Route::put('{id}', [StageParticipantsController::class, 'update']); // PUT /api/participants/{id}
     Route::delete('{id}', [StageParticipantsController::class, 'destroy']); // DELETE /api/participants/{id}
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
 
